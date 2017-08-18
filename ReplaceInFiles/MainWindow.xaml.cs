863b55e998305f2d46a19f262bec5ceab570fddb
Lisa Dong <ldong@logitech.com>
2017-08-18 14:32:03 +0800

    Finish basic functions.

--- ReplaceInFiles/MainWindow.xaml.cs
+++ ReplaceInFiles/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,62 @@ namespace ReplaceInFiles
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<FindReplacePair> replaceList
+            = new ObservableCollection<FindReplacePair>();
+        ObservableCollection<string> fileList = new ObservableCollection<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            dataGrid.ItemsSource = replaceList;
+            listBox.ItemsSource = fileList;
+            replaceList.Add(new FindReplacePair("1", "2"));
         }
+
+        private void buttonAddRow_Click(object sender, RoutedEventArgs e)
+        {
+            replaceList.Add(new FindReplacePair());
+        }
+
+        private void buttonSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                    fileList.Add(fileName);
+            }
+        }
+
+        private void buttonReplace_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string fileName in fileList)
+            {
+                string text = File.ReadAllText(fileName);
+                foreach (FindReplacePair pair in replaceList)
+                {
+                    text = text.Replace(pair.Find, pair.Replace);
+                }
+                File.WriteAllText(fileName, text);
+            }
+            MessageBox.Show("Done!");
+        }
+    }
+
+    public class FindReplacePair
+    {
+        public FindReplacePair()
+        {
+            Find = string.Empty;
+            Replace = string.Empty;
+        }
+        public FindReplacePair(string find, string replace)
+        {
+            Find = find;
+            Replace = replace;
+        }
+        public string Find { get; set; }
+        public string Replace { get; set; }
     }
 }
